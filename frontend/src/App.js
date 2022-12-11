9b4d33e370eefb7a4c017364d261c767b368684b
Dodley <dodleydesign+github@gmail.com>
2022-12-11 20:12:14 +0100

    Finished with sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import Sidebar from "./components/sidebar";
 
 export default class App extends Component {
   render() {
-    return <Sidebar />;
+    return (
+      <div className="flex">
+        <Sidebar />
+        <h1 className="text-4xl">Header for test</h1>
+      </div>
+    );
   }
 }
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -12,7 +12,7 @@ export default class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: true,
+      open: false,
       active: 0,
     };
   }
@@ -21,12 +21,12 @@ export default class Sidebar extends Component {
     {
       name: "Dashboard",
       link: "#",
-      icon: <MdDashboard className="text-xl" />,
+      icon: <MdDashboard className="text-2xl" />,
     },
     {
       name: "Tasks",
       link: "#",
-      icon: <MdFormatListBulleted className="text-xl" />,
+      icon: <MdFormatListBulleted className="text-2xl " />,
     },
   ];
   toggleActive = (idx) => {
@@ -35,42 +35,23 @@ export default class Sidebar extends Component {
 
   toggleOpen = () => {
     this.setState({ open: !this.state.open });
-    let sidebar = document.getElementById("sidebar");
   };
-
   render() {
     return (
-      <div className="hidden lg:block">
+      <div className="hidden sm:block absolute lg:relative z-10 bg-white">
         <div
           className={
             this.state.open
-              ? "w-64 border overflow-hidden h-screen py-4 border-gray-300 px-4"
-              : "border w-16 overflow-hidden h-screen py-4 border-gray-300 px-4"
+              ? "w-64 border hidden sm:block transition-[width] duration-500 ease-in-out overflow-hidden h-screen py-4 border-gray-300 px-4"
+              : "border w-20 transition-[width] duration-500 ease-in-out overflow-hidden h-screen py-4 border-gray-300 px-4"
           }
         >
-          <div className="flex flex-col divide-y-2">
-            <div className="flex space-x-6 mb-12">
-              <img src={Logo} alt="logo" />
-              <span>Tasky</span>
+          <div className="flex flex-col h-full">
+            <div className="flex space-x-6 mb-12 items-baseline">
+              <img src={Logo} alt="logo" className="ml-1.5" />
+              <span className="font-bold text-xl uppercase px-4">Tasky</span>
             </div>
             <div className="flex flex-col space-y-8">
-              <div className="py-2 flex justify-center">
-                {this.state.open ? (
-                  <button
-                    className="p-2 text-center text-gray-700"
-                    onClick={() => this.toggleOpen()}
-                  >
-                    <MdArrowBackIos />
-                  </button>
-                ) : (
-                  <button
-                    className="p-2 rounded-md  text-gray-700"
-                    onClick={() => this.toggleOpen()}
-                  >
-                    <MdArrowForwardIos />
-                  </button>
-                )}
-              </div>
               {this.LINKS.map((val, idx, arr) => {
                 return (
                   <a
@@ -83,21 +64,41 @@ export default class Sidebar extends Component {
                     }
                     onClick={() => this.toggleActive(idx)}
                   >
-                    <i className="hover:text-primary-200 transition-colors duration-200">
+                    <i className="hover:text-primary-200 transition-colors duration-400 p-2">
                       {val.icon}
                     </i>
                     <span>{val.name}</span>
                   </a>
                 );
               })}
-
-              <a href="/" className="flex space-x-8 items-center mt-auto">
-                <i className="hover:text-primary-200 transition-colors duration-200">
-                  <MdAccountCircle className="text-2xl" />
-                </i>
-                <span>Account</span>
-              </a>
+              <div className="py-2 flex justify-end">
+                {this.state.open ? (
+                  <button
+                    className="p-4 text-gray-700"
+                    onClick={() => this.toggleOpen()}
+                  >
+                    <MdArrowBackIos />
+                    <div className="inline "></div>
+                  </button>
+                ) : (
+                  <button
+                    className="p-4 rounded-md  text-gray-700"
+                    onClick={() => this.toggleOpen()}
+                  >
+                    <MdArrowForwardIos />
+                  </button>
+                )}
+              </div>
             </div>
+            <a
+              href="#"
+              className="flex space-x-8 items-center mt-auto justfy-self-end bg-primary-300 hover:bg-primary-200 transition duration-400 p-2 py-2.5 rounded-[48px]"
+            >
+              <i className="hover:text-primary-200 ml-0.5 transition-colors duration-200">
+                <MdAccountCircle className="text-2xl text-white" />
+              </i>
+              <span className="text-white">Account</span>
+            </a>
           </div>
         </div>
       </div>
